fab0205bdeaa139f09094d077bc1e4df7f331175
vinayreddy938 <vinaykumarreddycse@gmail.com>
2026-01-04 18:17:30 +0530

    connected to postgress database using prisma and generated table creation migrations

---
+++ backend/prisma/migrations/20260104124614_created_user_and_course_databases/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Course" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Course_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import { PORT } from "./config/server.config.js";
+import authRouter from "./routes/auth.router.js";
+import courseRouter from "./routes/course.router.js";
 
 const app = express();
 
-
+app.use("/api/v1/user",authRouter);
+app.use("/api/v1/course",courseRouter);
 
 app.listen(PORT,()=>{
     console.log("server started on port",PORT);
